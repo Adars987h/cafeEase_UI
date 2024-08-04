1f6ee65e065549a4a069fea90f2c1d1e233a4509
Abhishek Srivastava <abhisri344@gmail.com>
2024-08-04 16:33:53 +0530

    cart empty handling

--- src/Pages/UserCartPage.js
+++ src/Pages/UserCartPage.js
@@ -2,7 +2,7 @@ import "../CSS/DashboardPage.css";
 import "../CSS/UserCartPage.css";
 import React from 'react';
 import Navbar from '../Components/DashboardPageComponents/Navbar';
-import Cart from '../Components/DashboardPageComponents/Cart';
+import Cart from '../Components/DashboardPageComponents/Cart copy';
 import Footer from '../Components/DashboardPageComponents/Footer';
 
 import { ToastContainer } from 'react-toastify';
--- src/Pages/UserProductsPage.js
+++ src/Pages/UserProductsPage.js
@@ -4,8 +4,6 @@ import React from 'react';
 import Navbar from '../Components/DashboardPageComponents/Navbar';
 import Home from '../Components/DashboardPageComponents/Home';
 import Products from '../Components/DashboardPageComponents/Products';
-import Orders from '../Components/DashboardPageComponents/Orders';
-import Cart from '../Components/DashboardPageComponents/Cart';
 import Footer from '../Components/DashboardPageComponents/Footer';
 
 // import { ToastContainer } from 'react-toastify';
